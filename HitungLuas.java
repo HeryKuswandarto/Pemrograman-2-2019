4b73031bcff324050fd4d31eea12f8fd9dd33ff1
HeryKuswandarto <haryjay@gmail.com>
2019-01-25 08:42:11 +0700

    Perubahan 2

--- HitungLuas.java
+++ HitungLuas.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+
 public class HitungLuas {
+    Scanner scan = new Scanner(System.in);
+    
+    public HitungLuas(){
+        cariLuasPersegiPanjang();
+        
+    }
+    public void cariLuasPersegiPanjang(){
+        System.out.println ("Menghitung Luas Persegi Panjang");
+        System.out.println ("-------------------------------");
+        System.out.print ("Masukkan panjang : ");
+        String inputpanjang = scan.nextLine();
+        int panjang = Integer.parseInt(inputpanjang);
+        System.out.print ("Masukkan lebar   : ");
+        String inputlebar = scan.nextLine();
+        int lebar = Integer.parseInt(inputlebar);
+        int luaspersegipanjang = panjang*lebar;
+        System.out.println ("Luas             : "+luaspersegipanjang);
+        System.out.println();
+    }
 
     public static void main(String[] args) {
-    }
-    
+        new HitungLuas();
+    }    
 }
